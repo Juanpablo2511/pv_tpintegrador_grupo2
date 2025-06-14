8a5f3d4532c8c29c9701e191d504ebea03e46ec0
Matifq14 <matias15963@hotmail.com>
2025-06-13 21:45:02 -0300

    estilos navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 import { Link } from 'react-router-dom'
-
+import '../css/Navbar.css'
 const Navbar = () => {
   return (
-    <nav style={{ padding: '1rem', background: '#f0f0f0' }}>
+    <nav className="navbar-custom" style={{ padding: '1rem', background: '#f0f0f0' }}>
       <Link to="/">Home</Link> |{' '}
       <Link to="/favoritos">Favoritos</Link> |{' '}
       <Link to="/crear">Crear Producto</Link>
