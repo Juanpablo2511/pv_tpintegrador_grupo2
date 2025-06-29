5d6ab30923243c8056d1ee6f864a71f9aa26bdd7
NadiaE-Fernandez <naniifer98@gmail.com>
2025-06-29 17:09:29 -0300

    Agrego el buscador

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,20 @@
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation, useNavigate} from 'react-router-dom'
+import {useState} from 'react';
+import { FaSearch } from 'react-icons/fa';
 
 export default function Navbar() {
+  const [searchTerm,setSearchTerm]= useState("");
+  const navigate= useNavigate();
   const location = useLocation();
 
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+    const term= searchTerm.trim();
+    if(term !== ""){
+      navigate(`/?search=${encodeURIComponent(term)}`);
+      setSearchTerm("");
+    }
+  }
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary px-4">
       <div className="container-fluid">
@@ -37,6 +49,15 @@ export default function Navbar() {
               </Link>
             </li>
           </ul>
+          {/* //Buscador */}
+          <form className="d-flex" onSubmit={handleSubmit}>
+            <div className="input-group">
+              <input type="search" className="form-control" placeholder='Buscar productos...' value={searchTerm} onChange={(e)=> setSearchTerm(e.target.value)} />
+              <button className='btn btn-light border' type="submit">
+                <FaSearch color='#6c757d' />
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </nav>
