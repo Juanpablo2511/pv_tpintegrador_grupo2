b688b39061152a3ca476419a0f85a0bfd9fdaf3e
Juan Pablo Farfan <juanpablofarfan234@gmail.com>
2025-06-29 21:35:36 -0300

    fix:navbar ahora es colapsable y funcional en vista mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,11 +19,26 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary px-4">
       <div className="container-fluid">
+        {/* Logo */}
         <Link className="navbar-brand fw-bold" to="/">
           🛒 TechStore
         </Link>
 
-        <div className="collapse navbar-collapse justify-content-end">
+        {/* Botón despegable */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        {/* Menú colapsable */}
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
               <Link 
@@ -62,6 +77,5 @@ export default function Navbar() {
         </div>
       </div>
     </nav>
-  )
+  );
 }
-
